575de0b8e04b29706e61c474299aec71f1353161
eugustavo61-spec <eugustavo61@gmail.com>
2026-05-18 15:22:32 -0300

    feature: adicionar clientes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 10;
+#define MAX 10
 char fila[MAX][50];
 int inicio = 0;
 int fim = 0;
+
+void adicionarCliente() {
+    if (fim == MAX) {
+        printf("Fila cheia!\n");
+        return;
+    }
+
+    printf("Nome do cliente: ");
+    scanf("%s", fila[fim]);
+
+    fim++;
+
+    printf("Cliente adicionado!\n");
+}
